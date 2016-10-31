374ea3e0f160d3291f96ccefd27b5e1fdf172af7
Bart van Strien <bart.bes@gmail.com>
2016-10-31 21:51:05 +0100

    Version 0.10.2 was released

--- main.js
+++ main.js
@@ -222,6 +222,7 @@ function versionpicker() {
 
 	function injectVersionTags () {
 		var versions = [
+			{"version": "0.10.2", "codename": "Super Toast"},
 			{"version": "0.10.1", "codename": "Super Toast"},
 			{"version": "0.10.0", "codename": "Super Toast"},
 			{"version": "0.9.2", "codename": "Baby Inspector"},
